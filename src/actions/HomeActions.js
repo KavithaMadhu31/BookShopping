5a7547b8388e1eff6cc3f52c4add6ea2f8f36078
Kavitha Madhu <kavitha.madhu@viamagus.com>
2021-12-18 11:59:20 +0530

    Actions & Reducers to fetch response

---
+++ src/actions/HomeActions.js
@@ -0,0 +1,7 @@
+import { FETCHING_DATA } from './ActionConstants'
+
+export function fetchData() {
+    return {
+        type: FETCHING_DATA
+    }
+}
--- src/services/HomeServices.js
+++ src/services/HomeServices.js
@@ -1,5 +1,9 @@
 import {GET_URL} from './Constants';
-
+import {
+  FETCHING_DATA,
+  FETCHING_DATA_SUCCESS,
+  FETCHING_DATA_FAILURE,
+} from 'src/actions/ActionConstants';
 export async function useFetchBookAPI(pageNo) {
   const url = GET_URL.BOOK_URL + `${pageNo}`;
   console.log('the url is', url);
@@ -11,4 +15,37 @@ export async function useFetchBookAPI(pageNo) {
     .catch(err => {
       return Promise.reject(err);
     });
+}
+
+export function fetchBookList(pageNo) {
+  const url = GET_URL.BOOK_URL + `${pageNo}`;
+  return dispatch => {
+    dispatch(getBookLists());
+
+    return fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        return dispatch(getBooksListSuccess(json));
+      })
+      .catch(err => dispatch(getBooksListFailure(err)));
+  };
+}
+
+function getBookLists() {
+  return {
+    type: FETCHING_DATA,
+  };
+}
+
+function getBooksListSuccess(data) {
+  return {
+    type: FETCHING_DATA_SUCCESS,
+    data,
+  };
+}
+
+function getBooksListFailure() {
+  return {
+    type: FETCHING_DATA_FAILURE,
+  };
 }
